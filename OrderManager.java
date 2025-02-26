531348778b6ffe0b2ca5c71776b5039a3e8c66b1
Reham-Alsaedi <rehamalsaedi22@gmail.com>
2025-02-26 21:51:58 +0300

    Update OrderManager.java

---
+++ OrderManager.java
@@ -0,0 +1,6 @@
+public class OrderManager {
+    public void processOrder() {
+        System.out.println("Processing order...");
+        System.out.println("Order verification in progress...");
+    }
+}
